9e62b669ed7370fb313f18f25620a250cb83c7f8
AnjaliBharwani <acksacks2405@gmail.com>
2024-01-31 01:38:53 +0530

    submitting code-Wed Jan 31 2024 01:38:51 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,43 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
 
+const App=()=>{
+    const[todo,setTodo]=useState("");
+    const[count,setCount]=useState(0);
+    const[list,setList]=useState([]);
+    const[item,setItem]=useState("");
+
+    function addToList(e){
+        if(item.length<5)return;
+        e.preventDefault();
+        setList([...list,item]);
+        setItem("");
+    }
+    return(
+        <div id="main">
+            <h1>My todos</h1>
+            <div id="todo-0">{todo}</div>
+            <button id="add-todo-btn" onClick={()=>setTodo("New Todo")}>Add Todo</button>
+            <hr/>
+            <div id="incr-cnt">Count: {count}</div>
+            <button onClick={()=>setCount(count+1)} id="incr-btn">+</button>
+            <h3 >Expensive Calculation</h3>
+            <p id="calc">{1000000000+count}</p>
+            <hr/>
+            <hr/>
+            <form onSubmit={addToList}>
+                <input placeholder="Add Skill" onChange={(e)=>setItem(e.target.value) } id="skill-input" value={item}/>
+                <button id="skill-btn">Add Skill</button>
+            </form>
+            <ul>
+                {
+                    list.map((ele,i)=>
+                    <li id="item-jumbotron" key={i}>{ele}</li>
+                    )
+                }
+            </ul>
+
+        </div>
+    )
+}
+
+export default App
